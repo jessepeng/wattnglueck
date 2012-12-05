542f79cedd1a2823364a70ffa04a9e397d91b226
Jan-Christopher Pien <pijk@zedat.fu-berlin.de>
2012-12-05 23:07:29 +0100

    Replaced homescreen

--- src/org/fu/swphcc/wattnglueck/HomeScreen.java
+++ src/org/fu/swphcc/wattnglueck/HomeScreen.java
@@ -1,15 +1,24 @@
 package org.fu.swphcc.wattnglueck;
 
-import android.os.Bundle;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class HomeScreen extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		Date today = new Date();
+		String todayString = new SimpleDateFormat("dd.MM.yyyy").format(today);
 		setContentView(R.layout.activity_home_screen);
+		TextView statusView = (TextView) findViewById(R.id.textStatus); 
+		String showDate = getString(R.string.home_status).replace("$date", todayString);
+		statusView.setText(showDate);
 	}
 
 	@Override
