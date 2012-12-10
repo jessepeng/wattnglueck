c826eca21d74f248f09732b7e024e46fd1f32862
ericz <eric.zetzsche@hotmail.de>
2012-12-10 13:40:46 +0100

    database access added

---
+++ src/org/fu/swphcc/wattnglueck/Database.java
@@ -0,0 +1,29 @@
+package org.fu.swphcc.wattnglueck;
+
+import java.io.FileNotFoundException;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class Database extends SQLiteOpenHelper{
+
+	private static final String DATABASE_NAME = "wattndata";
+	private static final String DATABASE_TABLE = "data";
+	private static final int DATABASE_VERSION = 1;
+	private static final String TABLE_CREATE = "CREATE TABLE " + DATABASE_TABLE + " (id INT, date TEXT, value REAL);";
+
+	public Database(Context ctx) {
+		 super(ctx, DATABASE_NAME, null, DATABASE_VERSION);
+	}
+	
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		db.execSQL(TABLE_CREATE);
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		
+	}
+}
