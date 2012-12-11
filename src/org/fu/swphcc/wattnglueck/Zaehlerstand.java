57e2af37d5b95a67d86711bd38aa1b2ae3f707a9
ericz <eric.zetzsche@hotmail.de>
2012-12-11 21:00:35 +0100

    Zaehlerstand.java added

---
+++ src/org/fu/swphcc/wattnglueck/Zaehlerstand.java
@@ -0,0 +1,24 @@
+package org.fu.swphcc.wattnglueck;
+
+import java.util.Date;
+
+public class Zaehlerstand {
+	
+	private Double Zaehlerstand;
+	private Date date;
+	
+	
+	
+	public Double getZaehlerstand() {
+		return Zaehlerstand;
+	}
+	public void setZaehlerstand(Double zaehlerstand) {
+		Zaehlerstand = zaehlerstand;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+}
